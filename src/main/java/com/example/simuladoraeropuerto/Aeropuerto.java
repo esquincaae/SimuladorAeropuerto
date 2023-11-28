9166bf6046a9ff878754eee417e76f31fd1ff924
LuisAxelMarínGarcía <213467@ids.upchiapas.edu.mx>
2023-11-28 12:27:13 -0600

    base del proyecto

--- src/main/java/com/example/simuladoraeropuerto/Aeropuerto.java
+++ src/main/java/com/example/simuladoraeropuerto/Aeropuerto.java
@@ -11,7 +11,7 @@ public class Aeropuerto extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Aeropuerto.class.getResource("vista-aeropuerto.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
